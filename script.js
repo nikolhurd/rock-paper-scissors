480e1384da8cf1bf6dfb15f2cb67feb63503e7ca
nikolhurd <nikolhurd@gmail.com>
2024-03-15 18:45:09 +0100

    function playGame, nested user propting inside this function

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 const getComputerMathChoice = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-var computerResponse = getComputerMathChoice(1, 3);
 
 /* Conditional statement for choosing rock, paper, scissors */
 const computerSelection = () => {
+  const computerResponse = getComputerMathChoice(1, 3);
   if (computerResponse === 1) {
     return "rock";
   } else if (computerResponse === 2) {
@@ -15,39 +15,47 @@ const computerSelection = () => {
   }
 };
 
-/* Takes inputs and transforms it into all lower case word*/
-let fullUserAnswer = prompt("What do you choose?");
-let userAnswer = fullUserAnswer.toLowerCase();
-
-/* Stores answer inside a variable */
-let playerSelection;
-if (
-  userAnswer === "rock" ||
-  userAnswer === "paper" ||
-  userAnswer === "scissors"
-) {
-  playerSelection = userAnswer;
-} else {
-  console.log("Invalid input. Choose rock, paper or scissors");
-}
-
 /* One round */
+let stat;
 const playRound = (playerSelection, computerSelection) => {
   if (playerSelection === "rock" && computerSelection === "paper") {
-    console.log("Computer wins! Paper beats rock");
+    stat = "Computer wins! Paper beats rock";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    console.log("You win! Paper beats rock");
+    stat = "You win! Paper beats rock";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    console.log("You win! Scissors beat paper");
+    stat = "You win! Scissors beat paper";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    console.log("Computer wins! Rock beat scissors");
+    stat = "Computer wins! Rock beat scissors";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    console.log("You win! Rock beat scissors");
+    stat = "You win! Rock beat scissors";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    console.log("Computer wins! Scissors beat paper");
+    stat = "Computer wins! Scissors beat paper";
   } else if (playerSelection === computerSelection) {
-    console.log("Its a tie!");
+    stat = "Its a tie!";
   }
 };
 
-playRound(playerSelection, computerSelection());
+function playGame() {
+  for (let i = 0; i <= 4; i++) {
+    /* Takes inputs and transforms it into all lower case word*/
+    let fullUserAnswer = prompt("What do you choose?");
+    let userAnswer = fullUserAnswer.toLowerCase();
+
+    /* Stores answer inside a variable */
+    let playerSelection;
+    if (
+      userAnswer === "rock" ||
+      userAnswer === "paper" ||
+      userAnswer === "scissors"
+    ) {
+      playerSelection = userAnswer;
+    } else {
+      console.log("Invalid input. Choose rock, paper or scissors");
+    }
+
+    let computerChoice = computerSelection();
+    playRound(playerSelection, computerChoice);
+    console.log(stat);
+  }
+}
+playGame();
